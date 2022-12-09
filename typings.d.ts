2d89b2828ced2ce336de7eaecd6e1e30fd177b7d
Jeyhun Farajov <97691925+jeyefendi@users.noreply.github.com>
2022-12-10 02:02:33 +0300

    type NewsResponse

--- typings.d.ts
+++ typings.d.ts
@@ -1,4 +1,16 @@
- type Category = 
+type Pagination = {
+  count: Int;
+  limit: Int;
+  offset: Int;
+  total: Int;
+}
+
+type NewsResponse = {
+  pagination: Pagination;
+  data: DataEntry[];
+}
+
+type Category = 
    | "general"
    | "business"
    | "entertainment"
