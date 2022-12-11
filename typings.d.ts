fc39dbc1ecb6b8262b645343628681d0e133c43f
Jeyhun Farajov <97691925+jeyefendi@users.noreply.github.com>
2022-12-11 15:30:50 +0300

    graphql added

--- typings.d.ts
+++ typings.d.ts
@@ -1,3 +1,16 @@
+type Article = {
+  author: string | null;
+  category: string;
+  country: string;
+  description: string;
+  image: string | null;
+  language: string;
+  published_at: string;
+  source: string;
+  title: string;
+  url: string;
+}
+
 type Pagination = {
   count: Int;
   limit: Int;
@@ -7,7 +20,7 @@ type Pagination = {
 
 type NewsResponse = {
   pagination: Pagination;
-  data: DataEntry[];
+  data: Article[];
 }
 
 type Category = 
